e3c899b7894f9278e179326779a24f505e59b857
Gusradika <aditezo24@gmail.com>
2024-10-09 10:18:25 +0700

    init

---
+++ app/Models/Section.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Classes;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Section extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        "class_id",
+        "name",
+    ];
+
+    public function Classes()
+    {
+        return $this->belongsTo(Classes::class);
+    }
+    public function students()
+    {
+        return $this->hasMany(Student::class, 'section_id');
+    }
+}
